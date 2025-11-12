f37c93b863e34b916cefcd0f9666defe11a792dc
rheinaadinda <moranisinurat@gmail.com>
2025-11-12 19:31:06 +0700

    Tutorial 8 selesai

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+
 import 'package:flutter/material.dart';
-import 'package:football_news/screens/menu.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:football_news/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,15 +11,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
- .copyWith(secondary: Colors.blueAccent[400]),
+    return Provider(
+      create: (_) {
+        CookieRequest request = CookieRequest();
+        return request;
+      },
+      child: MaterialApp(
+        title: 'Football News',
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
+          .copyWith(secondary: Colors.blueAccent[400]),
+        ),
+        home: const LoginPage(),
       ),
-      home: MyHomePage(),
     );
   }
 }
